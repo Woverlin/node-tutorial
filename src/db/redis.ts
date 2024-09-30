27a477b2698331464a61b6ffbdca476cf838a544
Hieu Truong <hieu.truong@pascal.studio>
2024-09-30 16:48:05 +0700

    update host

--- src/db/redis.ts
+++ src/db/redis.ts
@@ -1,19 +1,19 @@
 import Redis from "ioredis";
 
 const connectRedis = async () => {
-    const client = new Redis({
-        host: "localhost",
-        port: 6379,
-    });
+  const client = new Redis({
+    port: 6379, // Redis port
+    host: "103.100.134.219", // Redis host
+  });
 
-    client.on("connect", () => {
-        console.log("Connected to Redis");
-    });
+  client.on("connect", () => {
+    console.log("Connected to Redis");
+  });
 
-    client.on("error", (err) => {
-        console.log("Redis error: ", err);
-    });
-    return client;
+  client.on("error", (err) => {
+    console.log("Redis error: ", err);
+  });
+  return client;
 };
 
 export default connectRedis;
